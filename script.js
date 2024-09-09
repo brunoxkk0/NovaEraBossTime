cdffda8c660ae2f601b1da95d701c8eaf618765d
Erick <erickfthomaz@gmail.com>
2024-09-09 15:17:31 -0300

    Audio de notificação quando o boss nasce novamente

--- script.js
+++ script.js
@@ -85,6 +85,10 @@ const doAlert = (boss) => {
         icon: "./src/logo.png"
     })
 
+    const audio = new Audio("./src/alert.wav");
+    audio.volume = 0.15;
+    audio.play();
+
     updateStatus(boss)
 }
 
